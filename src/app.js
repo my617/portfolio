50395a300b80341f00a5ba83c5d20a68d6b02d17
5e302d1483d74c0e8217f5a5 <inujmai@gmail.com>
2020-04-03 10:14:18 +0900

    change the css

--- src/app.js
+++ src/app.js
@@ -13,14 +13,13 @@ $(function (){
     //モーダル
     $('.works__item').on('click', function () {
         var modalIndex = $(this).index();
-        $('#modal').eq(modalIndex).fadeIn();
+        $('.modal__wrapper').eq(modalIndex).fadeIn();
         $('.modal__cover').fadeIn();
 
         //モーダルの位置を中央にする
         function modalResize() {
             var windowWidth = $(window).width();
             var windowHeight = $(window).height();
-
             var modalWidth = (windowWidth - $(modal).outerWidth(true)) / 2;
             var modalHeight = (windowHeight - $(modal).outerHeight(true)) / 2;
 
@@ -31,17 +30,14 @@ $(function (){
         $(modal).fadeIn();
 
         $('.modal__close, .modal__cover').off().click(function () {
-            $('#modal').fadeOut();
-            $('.modal__cover').fadeOut('slow', function () {
-                $('.modal__cover').remove();
+            $('.modal__wrapper').fadeOut();
+            $('.modal__cover').fadeOut();
             });
         });
 
         $(window).on('resize', function () {
             modalResize();
         });
-    });
-
     //スクロール
     $(window).scroll(function () {
         scroll_effect();
